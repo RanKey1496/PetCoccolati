3ddc878d013b6c6cbf35eef6beaae4e7a698d620
RanKey1496 <jhongil96@gmail.com>
2015-10-17 02:32:41 -0500

    Version 0.1

---
+++ PetCoccolati/src/com/petcoccolati/controllers/MainCtl.java
@@ -0,0 +1,29 @@
+package com.petcoccolati.controllers;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.Executions;
+import org.zkoss.zk.ui.event.Event;
+import org.zkoss.zk.ui.util.GenericForwardComposer;
+import org.zkoss.zul.Listitem;
+import org.zkoss.zul.ListitemRenderer;
+import org.zkoss.zul.Window;
+
+public class MainCtl extends GenericForwardComposer implements ListitemRenderer{
+	
+	public void doAfterCompose(Component comp) throws Exception {
+		super.doAfterCompose(comp);
+	}
+
+	@Override
+	public void render(Listitem arg0, Object arg1, int arg2) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void onCreate() {
+		 
+	}	
+}
