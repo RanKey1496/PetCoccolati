9de857f529191894dc8e7861dc326fdbef3d71a3
RanKey1496 <jhongil96@gmail.com>
2015-10-18 20:34:52 -0500

    Login

--- PetCoccolati/src/com/petcoccolati/controllers/MainCtl.java
+++ PetCoccolati/src/com/petcoccolati/controllers/MainCtl.java
@@ -3,6 +3,8 @@ package com.petcoccolati.controllers;
 import java.io.IOException;
 import java.util.HashMap;
 
+import org.zkoss.bind.annotation.Command;
+import org.zkoss.bind.annotation.NotifyChange;
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.Executions;
 import org.zkoss.zk.ui.event.Event;
@@ -12,7 +14,7 @@ import org.zkoss.zul.ListitemRenderer;
 import org.zkoss.zul.Window;
 
 public class MainCtl extends GenericForwardComposer implements ListitemRenderer{
-	
+
 	public void doAfterCompose(Component comp) throws Exception {
 		super.doAfterCompose(comp);
 	}
@@ -26,4 +28,19 @@ public class MainCtl extends GenericForwardComposer implements ListitemRenderer{
 	public void onCreate() {
 		 
 	}	
+	
+	/*public void onClick$btnLogin(Event ev) {
+		java.io.InputStream zulInput = this.getClass().getClassLoader().getResourceAsStream("com/petcoccolati/views/login.zul") ;
+		java.io.Reader zulReader = new java.io.InputStreamReader(zulInput);
+		try {
+			Component c = Executions.createComponentsDirectly(zulReader,"zul",null,new HashMap<String, Object>()) ;
+			Window win = (Window)c;
+			win.doModal();
+			System.out.println("despues del do");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
+	}*/
+	
 }
