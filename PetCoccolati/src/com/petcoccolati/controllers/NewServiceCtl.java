7edcedb17bcc0961571d0615252df7d114954896
RanKey1496 <jhongil96@gmail.com>
2015-11-19 00:51:58 -0500

    SATAN HIJUEPUTA

---
+++ PetCoccolati/src/com/petcoccolati/controllers/NewServiceCtl.java
@@ -0,0 +1,19 @@
+package com.petcoccolati.controllers;
+
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.event.Event;
+import org.zkoss.zk.ui.util.GenericForwardComposer;
+import org.zkoss.zul.Button;
+import org.zkoss.zul.ListModelList;
+import org.zkoss.zul.Listbox;
+
+public class NewServiceCtl extends GenericForwardComposer{
+  	
+  	public void doAfterCompose(Component comp) throws Exception {
+		super.doAfterCompose(comp);
+
+	}
+  
+  	public void onClick$btn(Event e) {
+  	}
+}
