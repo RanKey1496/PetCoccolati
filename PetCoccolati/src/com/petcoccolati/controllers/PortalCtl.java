1c3136f5ea102fb9274c78325bf0d58b5181db15
RanKey1496 <jhongil96@gmail.com>
2015-11-20 22:02:40 -0500

    Portal

---
+++ PetCoccolati/src/com/petcoccolati/controllers/PortalCtl.java
@@ -0,0 +1,42 @@
+package com.petcoccolati.controllers;
+
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.Executions;
+import org.zkoss.zk.ui.event.Event;
+import org.zkoss.zk.ui.util.GenericForwardComposer;
+import org.zkoss.zul.Include;
+
+public class PortalCtl extends GenericForwardComposer implements PortalIntCtl{
+	
+	private Include innerIncld;
+	
+	public void doAfterCompose(Component comp) throws Exception {
+		super.doAfterCompose(comp);
+	}
+	
+	public PortalCtl(){
+		
+	}
+	
+	public void onClick$lblHome(Event e) {
+		innerIncld.setSrc("home.zul");
+	}
+	public void onClick$lblMyPets(Event e) {
+		innerIncld.setSrc("mypet.zul");
+	}
+	public void onClick$lblNewPet(Event e) {
+		innerIncld.setSrc("newpet.zul");
+	}
+	public void onClick$lblNewService(Event e) {
+		innerIncld.setSrc("newservice.zul");
+	}
+	public void onClick$lblHistory(Event e) {
+		innerIncld.setSrc("home.zul");
+	}
+	public void onClick$lblSettings(Event e) {
+		innerIncld.setSrc("settings.zul");
+	}
+	public void onClick$lblLogOut(Event e) {
+		Executions.sendRedirect("index.zul");
+	}
+}
---
+++ PetCoccolati/src/com/petcoccolati/controllers/PortalIntCtl.java
@@ -0,0 +1,9 @@
+package com.petcoccolati.controllers;
+
+import org.zkoss.zk.ui.Component;
+
+public interface PortalIntCtl {
+
+	public void doAfterCompose(Component comp) throws Exception;
+	
+}
